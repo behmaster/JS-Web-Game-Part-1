41caa8dc6cacb02cc5a6e72ffa5cd250ae993a57
Robbie Behm <behmaster@gmail.com>
2022-04-18 22:27:36 -0500

    Web Game pt1 HW

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(photoLocation, left, bottom) {
+  let object = document.createElement("img");
+  object.src = photoLocation;
+  object.style.position = "fixed";
+  object.style.left = left + "px";
+  object.style.bottom = bottom + "px";
+  document.body.append(object);
+  return object;
+}
+
+function newItem(itemLocation, left, bottom) {
+  let object = newImage(itemLocation, left, bottom);
+  object.addEventListener("dblclick", function () {
+    object.remove();
+  });
+}
+
+newImage("assets/green-character.gif", 100, 100);
+newImage("assets/pine-tree.png", 450, 200);
+newImage("assets/pillar.png", 350, 100);
+newImage("assets/crate.png", 150, 200);
+newImage("assets/well.png", 500, 425);
+
+newItem("assets/sword.png", 500, 405);
+newItem("assets/shield.png", 165, 185);
+newItem("assets/staff.png", 600, 100);
